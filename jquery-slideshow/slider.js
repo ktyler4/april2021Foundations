d012fbfe07a5de10d258f05c77298fec7b6a3473
ktyler4 <kennytyler.design@gmail.com>
2021-06-03 20:20:42 -0500

    fancy website june 3rd

---
+++ jquery-slideshow/slider.js
@@ -0,0 +1,96 @@
+function simpleSlider(element = "#simple-slider", auto = false, pause) {
+    
+    console.log("simpleSlider entering");
+
+    // Get parent element
+    var $this = $(element);
+
+    // Slides Container
+    var slidesCont = $this.children('.slides-container');
+    // Get all slides
+    var slides = slidesCont.children('.slide');
+
+    // Get pager div
+    var pager = $this.children('.pager');
+
+    // Get Previous / Next links
+    var arrowsCont = $this.children('.arrows');
+    var prevSlide = arrowsCont.children('.prev');
+    var nextSlide = arrowsCont.children('.next');
+
+    // Total slides count
+    var slidesCount = slides.length;
+
+    // Set currentSlide to first child
+    var currentSlide = slides.first();
+    var currentSlideIndex = 1;
+
+    var autoPlay = null;
+
+    // Hide all slides except first and add active class to first
+    slides.not(':first').css('display', 'none');
+    currentSlide.addClass('active');
+    
+
+    function fadeNext() {
+        currentSlide.removeClass('active').fadeOut(700);
+
+        if (currentSlideIndex == slidesCount) {
+            currentSlide = slides.first();
+            currentSlide.delay(500).addClass('active').fadeIn(700);
+            currentSlideIndex = 1;
+        } else {
+            currentSlideIndex++;
+            currentSlide = currentSlide.next();
+            currentSlide.delay(500).addClass('active').fadeIn(700);
+        }
+
+        pager.text(currentSlideIndex + ' / ' + slidesCount);
+    }
+
+    // Function responsible for fading to previous slide
+    function fadePrev() {
+        currentSlide.removeClass("active").fadeOut(700);
+
+        if (currentSlideIndex == 1) {
+            currentSlide = slides.last();
+            currentSlide.delay(500).addClass('active').fadeIn();
+            currentSlideIndex = slidesCount;
+        } else {
+            currentSlideIndex--;
+            currentSlide = currentSlide.prev();
+            currentSlide.delay(500).addClass('active').fadeIn(700);
+        }
+
+        pager.text(currentSlideIndex + ' / ' + slidesCount);
+    }
+
+    // Function that starts the autoplay and resets it in case user navigated (clicked prev or next)
+    function AutoPlay() {
+        clearInterval(autoPlay);
+
+        if (auto == true)
+        autoPlay = setInterval(function() {fadeNext() }, pause);
+    }
+
+    // Detect if user clicked on arrow for tnext slide and fade next slide if it did
+    $(nextSlide).click(function(e) {
+        e.preventDefault();
+        fadeNext();
+        AutoPlay();
+    });
+
+    // Detect if user clicked on arrow for previous slide and fade previous slide if it did
+    $(prevSlide).click(function(e) {
+        e.preventDefault();
+        fadePrev();
+        AutoPlay();
+    });
+
+    //  Start autoplay if auto is set to true
+    AutoPlay();
+}
+
+$(function() {
+    simpleSlider('#slider' , true, 8000);
+});
